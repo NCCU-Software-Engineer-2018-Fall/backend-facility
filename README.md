bf211bf668758b8e652981b2174a3ad037af24fe
polarjack <luckyjack85@gmail.com>
2018-05-14 16:06:41 +0800

    ignore apidoc build files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-this is a database schema / migration / tools service
+**Facility System api**
+----
+* yarn start: normal start without watch
+* yarn pm2-with-watch: start with watch (dev purpose port 9000)
+* yarn apidoc-generate: generate apidoc
+* yarn apidoc-open: apidoc is a web (port 10800)
---
+++ src/sample/user.js
@@ -0,0 +1,58 @@
+const fetch = require('node-fetch');
+const endPoint = require('./config');
+
+/**
+ * @api {get} /user/all Get All User
+ * @apiName GetAllUser
+ * @apiGroup User
+ *
+ * @apiParam None.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "status": "success",
+ *       "data": [],
+ *     }
+ *
+ * @apiError UserNotFound The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "UserNotFound"
+ *     }
+ */
+
+fetch(endPoint + '/user/all')
+  .then(res => res.text())
+  .then(body => console.log(body));
+
+/**
+ * @api {get} /user/:id Get User By Id
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "status": "success",
+ *       "data": [],
+ *     }
+ *
+ * @apiError UserNotFound The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "UserNotFound"
+ *     }
+ */
