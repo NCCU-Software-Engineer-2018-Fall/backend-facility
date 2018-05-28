34cf8ddca5ce5de8fb4c8a8b997b67d53daedf31
polarjack <luckyjack85@gmail.com>
2018-05-28 15:48:43 +0800

    update apidoc appointment

--- src/sample/appointment.js
+++ src/sample/appointment.js
@@ -248,8 +248,224 @@ const endPoint = require('./config');
  */
 
 function getAllAppointment() {
-  console.log('GET all Classrooms');
+  console.log('GET all Appointment');
   fetch(endPoint + '/appointment/all')
+    .then(res => res.text())
+    .then(body => {
+      console.log(body);
+      console.log('END');
+    });
+}
+
+/**
+ * @api {get} /appointment/query/byUser/:user_id Get Appointment By User
+ * @apiName get appointment by user_id
+ * @apiGroup Appointment
+ *
+ * @apiParam {String} user_id Users unique ID.
+ *
+ * @apiSuccess {Object[]} data array of Appointment.
+ * @apiSuccess {String} id id of the Appointment (pk).
+ * @apiSuccess {String} appointment_id id of the Appointment (AI).
+ * @apiSuccess {String} user_id id of the User (pk).
+ * @apiSuccess {String} classroom_id id of the Classroom (pk).
+ * @apiSuccess {String} period_id id of the Period (pk).
+ * @apiSuccess {Date} reserved_date Date of the Appointment.
+ * @apiSuccess {String} classroom_name of the Classroom (Unique).
+ * @apiSuccess {String} [building] located building of the Classroom.
+ * @apiSuccess {String} [floor] floor of the Classrooom.
+ * @apiSuccess {String} period_id id of the Period (AI).
+ * @apiSuccess {String} symbol symbol of the Period.
+ * @apiSuccess {String} period_name name of the Period (Unique).
+ * @apiSuccess {Time} start_time start time of the Period (Unique).
+ * @apiSuccess {Time} end_time end time of the Period (Unique).
+ * @apiSuccess {Char} status of the Appointment (For db).
+ * @apiSuccess {TimeStamps} creation_time of the data (For db).
+ *
+ *
+ * @apiSuccessExample Success-Response:
+ *  HTTP/1.1 200 OK
+ *  {
+ *    "status": "success",
+ *    "data": [
+ *      {
+ *        "id": "2bdc686b-37d6-4f71-80d1-49afd67cfed3",
+ *        "appointment_id": 4,
+ *        "user_id": "2bdc686b-37d6-4f71-80d1-49afd67cfed3",
+ *        "classroom_id": "ed5bb09b-b535-4b94-98e0-6be8af4019b1",
+ *        "period_id": "9aa22837-fc46-4581-93ba-efb27ef80df1",
+ *        "reserved_date": "2018-05-08",
+ *        "status": "1",
+ *        "creation_time": "2018-05-09T13:46:54.326Z",
+ *        "hash_check": "6e3233a69fa3e57dc09643a5636c0072",
+ *        "classroom_name": "大仁樓200101",
+ *        "building": null,
+ *        "floor": null,
+ *        "state": "1",
+ *        "symbol": "1",
+ *        "period_name": "0810~0900",
+ *        "start_time": "08:10:00",
+ *        "end_time": "09:00:00",
+ *        "student_id": "104703030",
+ *        "student_name": "Jack Yu"
+ *      },
+ *    ]
+ *  }
+ *
+ */
+
+function getAppointmentByUser() {
+  console.log('GET Appointment by User');
+  let user_id = '2660d148-a3bc-464f-8238-aefe4197326c';
+  fetch(endPoint + '/appointment/query/byUser/' + user_id)
+    .then(res => res.text())
+    .then(body => {
+      console.log(body);
+      console.log('END');
+    });
+}
+
+/**
+ * @api {get} /appointment/query/byClassroom/:classroom_id Get Appointment By User
+ * @apiName get appointment by classroom_id
+ * @apiGroup Appointment
+ *
+ * @apiParam {String} user_id User unique ID.
+ * @apiParam {String} classroom_id Classroom unique ID.
+ *
+ * @apiSuccess {Object[]} data array of Appointment.
+ * @apiSuccess {String} id id of the Appointment (pk).
+ * @apiSuccess {String} appointment_id id of the Appointment (AI).
+ * @apiSuccess {String} user_id id of the User (pk).
+ * @apiSuccess {String} classroom_id id of the Classroom (pk).
+ * @apiSuccess {String} period_id id of the Period (pk).
+ * @apiSuccess {Date} reserved_date Date of the Appointment.
+ * @apiSuccess {String} classroom_name of the Classroom (Unique).
+ * @apiSuccess {String} [building] located building of the Classroom.
+ * @apiSuccess {String} [floor] floor of the Classrooom.
+ * @apiSuccess {String} period_id id of the Period (AI).
+ * @apiSuccess {String} symbol symbol of the Period.
+ * @apiSuccess {String} period_name name of the Period (Unique).
+ * @apiSuccess {Time} start_time start time of the Period (Unique).
+ * @apiSuccess {Time} end_time end time of the Period (Unique).
+ * @apiSuccess {Char} status of the Appointment (For db).
+ * @apiSuccess {TimeStamps} creation_time of the data (For db).
+ *
+ *
+ * @apiSuccessExample Success-Response:
+ *  HTTP/1.1 200 OK
+ *  {
+ *    "status": "success",
+ *    "data": [
+ *      {
+ *        "id": "2bdc686b-37d6-4f71-80d1-49afd67cfed3",
+ *        "appointment_id": 4,
+ *        "user_id": "2bdc686b-37d6-4f71-80d1-49afd67cfed3",
+ *        "classroom_id": "ed5bb09b-b535-4b94-98e0-6be8af4019b1",
+ *        "period_id": "9aa22837-fc46-4581-93ba-efb27ef80df1",
+ *        "reserved_date": "2018-05-08",
+ *        "status": "1",
+ *        "creation_time": "2018-05-09T13:46:54.326Z",
+ *        "hash_check": "6e3233a69fa3e57dc09643a5636c0072",
+ *        "classroom_name": "大仁樓200101",
+ *        "building": null,
+ *        "floor": null,
+ *        "state": "1",
+ *        "symbol": "1",
+ *        "period_name": "0810~0900",
+ *        "start_time": "08:10:00",
+ *        "end_time": "09:00:00",
+ *        "student_id": "104703030",
+ *        "student_name": "Jack Yu"
+ *      },
+ *    ]
+ *  }
+ *
+ */
+
+function getAppointmentByClassroom() {
+  console.log('GET Appointment by Classroom');
+  let classroom_id = 'a33110b8-6766-448b-9107-d4ce8ca710d4';
+  fetch(endPoint + '/appointment/query/byClassroom/' + classroom_id)
+    .then(res => res.text())
+    .then(body => {
+      console.log(body);
+      console.log('END');
+    });
+}
+
+/**
+ * @api {post} /appointment/query/byUserAndClassroom Get Appointment By User And Classroom
+ * @apiName get appointment by user_id and classroom_id
+ * @apiGroup Appointment
+ *
+ * @apiParam {String} classroom_id Classroom unique ID.
+ *
+ * @apiSuccess {Object[]} data array of Appointment.
+ * @apiSuccess {String} id id of the Appointment (pk).
+ * @apiSuccess {String} appointment_id id of the Appointment (AI).
+ * @apiSuccess {String} user_id id of the User (pk).
+ * @apiSuccess {String} classroom_id id of the Classroom (pk).
+ * @apiSuccess {String} period_id id of the Period (pk).
+ * @apiSuccess {Date} reserved_date Date of the Appointment.
+ * @apiSuccess {String} classroom_name of the Classroom (Unique).
+ * @apiSuccess {String} [building] located building of the Classroom.
+ * @apiSuccess {String} [floor] floor of the Classrooom.
+ * @apiSuccess {String} period_id id of the Period (AI).
+ * @apiSuccess {String} symbol symbol of the Period.
+ * @apiSuccess {String} period_name name of the Period (Unique).
+ * @apiSuccess {Time} start_time start time of the Period (Unique).
+ * @apiSuccess {Time} end_time end time of the Period (Unique).
+ * @apiSuccess {Char} status of the Appointment (For db).
+ * @apiSuccess {TimeStamps} creation_time of the data (For db).
+ *
+ *
+ * @apiSuccessExample Success-Response:
+ *  HTTP/1.1 200 OK
+ *  {
+ *    "status": "success",
+ *    "data": [
+ *      {
+ *        "id": "2bdc686b-37d6-4f71-80d1-49afd67cfed3",
+ *        "appointment_id": 4,
+ *        "user_id": "2bdc686b-37d6-4f71-80d1-49afd67cfed3",
+ *        "classroom_id": "ed5bb09b-b535-4b94-98e0-6be8af4019b1",
+ *        "period_id": "9aa22837-fc46-4581-93ba-efb27ef80df1",
+ *        "reserved_date": "2018-05-08",
+ *        "status": "1",
+ *        "creation_time": "2018-05-09T13:46:54.326Z",
+ *        "hash_check": "6e3233a69fa3e57dc09643a5636c0072",
+ *        "classroom_name": "大仁樓200101",
+ *        "building": null,
+ *        "floor": null,
+ *        "state": "1",
+ *        "symbol": "1",
+ *        "period_name": "0810~0900",
+ *        "start_time": "08:10:00",
+ *        "end_time": "09:00:00",
+ *        "student_id": "104703030",
+ *        "student_name": "Jack Yu"
+ *      },
+ *    ]
+ *  }
+ *
+ */
+
+function getAppointmentByUserAndClassroom() {
+  console.log('GET Appointment by User And Classroom');
+
+  let data = {
+    user_id: '2660d148-a3bc-464f-8238-aefe4197326c',
+    classroom_id: 'a33110b8-6766-448b-9107-d4ce8ca710d4',
+  };
+
+  fetch(endPoint + '/appointment/query/byUserAndClassroom', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
     .then(res => res.text())
     .then(body => {
       console.log(body);
